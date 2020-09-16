d80ad661c930e20714cfa8d5f7ae15336041406d
LockStatic <31453209+LockStatic@users.noreply.github.com>
2020-09-15 21:59:09 -0700

    initial commit

---
+++ lockstatic_portfolio/src/main/java/com/garrett/lockstatic_portfolio/LockstaticPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.garrett.lockstatic_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockstaticPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockstaticPortfolioApplication.class, args);
+	}
+
+}
---
+++ lockstatic_portfolio/src/test/java/com/garrett/lockstatic_portfolio/LockstaticPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garrett.lockstatic_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockstaticPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
